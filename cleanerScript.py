6d52caab4099d7be52dd551bd4197a304dfe0854
alexAbela <s175124@student.dtu.dk>
2020-12-01 14:39:01 +0100

    Initial commit

--- cleanerScript.py
+++ cleanerScript.py
@@ -6,3 +6,9 @@ def clean(df)
 
     cleanTweets = [tc.tweet_cleaner_updated(text) for text in tweetList]
 
+    languageDf = sqlContext.createDataFrame([(l,) for l in languages], ['languages'])
+    df = df.withColumn("row_idx", row_number().over(Window.orderBy(monotonically_increasing_id())))
+    languageDf = languageDf.withColumn("row_idx", row_number().over(Window.orderBy(monotonically_increasing_id())))
+    df = df.join(languageDf, df.row_idx == languageDf.row_idx).drop("row_idx")
+    df = df.filter(df.languages == 'english').drop("languages")
+
